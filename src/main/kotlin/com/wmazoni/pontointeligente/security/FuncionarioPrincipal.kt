f7b0dca3f477ebddd49408162bad4090923cdbbc
Wellington Mazoni <wmazoni@protonmail.com>
2022-01-20 23:48:58 -0300

    Adicionando configurações do spring security

---
+++ src/main/kotlin/com/wmazoni/pontointeligente/security/FuncionarioPrincipal.kt
@@ -0,0 +1,29 @@
+package com.wmazoni.pontointeligente.security
+
+import com.wmazoni.pontointeligente.documents.Funcionario
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class FuncionarioPrincipal(val funcionario: Funcionario) : UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        val authorities: MutableCollection<GrantedAuthority> = mutableListOf<GrantedAuthority>()
+        authorities.add(SimpleGrantedAuthority(funcionario.perfil.toString()))
+        return authorities
+    }
+
+    override fun isEnabled(): Boolean = true
+
+    override fun getUsername(): String = funcionario.email
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun getPassword(): String = funcionario.senha
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+}
+
